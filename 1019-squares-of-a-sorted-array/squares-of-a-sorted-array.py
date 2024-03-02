54346e88667dc623e8bb6846b30b4be4a7fe9144
ayesheepatra <ayeshee04@gmail.com>
2024-03-02 18:43:08 -0500

    Time: 159 ms (44.98%) | Memory: 18.9 MB (40.42%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,5 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-       new_list = [num*num for num in nums]
-       #new_list.sort()
-       return sorted(new_list)
+        n = len(nums)
+        ans = [0] * n
+        start, end = 0, n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[start]) >= abs(nums[end]):
+                ans[i] = nums[start] * nums[start]
+                start += 1
+            else:
+                ans[i] = nums[end] * nums[end]
+                end -= 1
+        return ans
+
+
+
+       
