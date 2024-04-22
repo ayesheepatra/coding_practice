a7c8bde68eeeebcebc09b4a325ca220f19a487cf
AYESHEE PATRA <ayeshee04@gmail.com>
2024-04-22 16:22:50 -0400

    Time: 790 ms (99.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select employee_id, department_id
+from employee
+where primary_flag='Y' 
+OR employee_id IN (select employee_id 
+from employee 
+group by employee_id 
+having count(department_id)<=1)
