ea2336fe2ad27eb7e442b5e37e2422d7dc75e22a
AYESHEE PATRA <ayeshee04@gmail.com>
2024-04-29 12:41:15 -0400

    Time: 658 ms (93.27%) | Memory: 15.1 MB (59.47%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -14,19 +14,19 @@ class Solution(object):
                 continue
             l=i+1
             r=n-1
-            while (l<r):
+            while(l<r):
                 summation=nums[i]+nums[l]+nums[r]
                 if summation>0:
                     r=r-1
                 elif summation<0:
                     l=l+1
                 else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l, r = l + 1, r - 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
+                    res.append([nums[i],nums[l],nums[r]])
+                    l=l+1
+                    r=r-1
+                    while l<r and nums[l]==nums[l-1]:
+                        l=l+1
+                    while l<r and nums[r]==nums[r+1]:
+                        r=r-1
         return res
 
-        
