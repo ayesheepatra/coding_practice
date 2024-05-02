9d4ea37194e3cd5b97d273367bc2d96c40ba85f3
AYESHEE PATRA <ayeshee04@gmail.com>
2024-05-01 21:10:53 -0400

    Time: 41 ms (16.84%) | Memory: 16.5 MB (37.60%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        pascal_matrix=[[1],[1,1]]
-        if rowIndex <  2:
-            return pascal_matrix[rowIndex]
-        
         pascal_matrix = [1, 1]
+        if rowIndex < 2:
+            return pascal_matrix[:rowIndex+1]
+        
         for i in range(2,rowIndex+1):
-            row=[1,]
-            for j in range(i-1):
-                row.append(pascal_matrix[j] + pascal_matrix[j+1])
-            row.append(1)
+            row=[1] + [pascal_matrix[j] + pascal_matrix[j+1] for j in range(i-1)] + [1]
+            # for j in range(i-1):
+            #     row.append(pascal_matrix[j] + pascal_matrix[j+1])
+            # row.append(1)
             pascal_matrix = row
         
         return pascal_matrix
-
         
