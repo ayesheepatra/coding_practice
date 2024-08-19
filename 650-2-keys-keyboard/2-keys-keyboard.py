dc10f88cd036793b15fea4c3acb84c3f455f8218
AYESHEE PATRA <ayeshee04@gmail.com>
2024-08-18 18:44:42 -0700

    Time: 39 ms (77.68%) | Memory: 16.6 MB (61.96%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,17 +1,15 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        curr = 1 # current number of 'A's on the screen
-        clipboard = 0 # number of 'A's in the clipboard
-        steps = 0 # number of steps taken to reach n 'A's
-        # loop until curr equals n
+        curr = 1
+        clip = 0
+        i = 0
         while curr < n:
-            # if n is divisible by curr, perform copy and paste operations
             if n % curr == 0:
-                clipboard = curr
-                steps += 1
-            # if n is not divisible by curr, perform only paste operation
-            curr += clipboard
-            steps += 1
-
-        return steps
+                i = i + 2
+                clip = curr
+                curr = curr + clip
+            else:
+                i = i + 1
+                curr = curr + clip
+        return i
         
